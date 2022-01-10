6a177f6812ebffd7d4ab713e4e2b78d7c60afef0
Pierre Fromager <info@pier-infor.fr>
2022-01-10 18:56:04 +0100

    test : CMake improvement to build hpp_lib to be testable

--- test/test.cpp
+++ test/test.cpp
@@ -1,38 +1,76 @@
 #define BOOST_TEST_MODULE HbpTests
 
 #include <boost/test/unit_test.hpp>
-// #include "../include/humangenders.h"
-// #include "../include/humanlimbs.h"
-#include "../include/humanratios.h"
+#include <iomanip>
+//#include "testbasefixture.h"
+#include "humanratios.h"
 
 BOOST_AUTO_TEST_SUITE(TestHumanRatios)
 
-struct TestHumanRatiosFixture
+struct TestBaseFixt
 {
-    TestHumanRatiosFixture() : i(0)
-    {
-        BOOST_TEST_MESSAGE("setup");
-        hr = new Human::Ratios(Human::Genders::Id::MALE, 0, 0);
-    }
-    ~TestHumanRatiosFixture()
-    {
-        delete hr;
-        BOOST_TEST_MESSAGE("teardown");
-    }
-    int i;
-    Human::Ratios *hr;
+    const std::string m_setup = "setup";
+    const std::string m_teardown = "teardown";
 };
 
-BOOST_FIXTURE_TEST_CASE(THRF1, TestHumanRatiosFixture)
+struct TestRatiosFixt : TestBaseFixt
 {
-    BOOST_CHECK_EQUAL(i, 0);
-    BOOST_CHECK_EQUAL(5, 5);
+    TestRatiosFixt(
+        unsigned int gender,
+        long double size,
+        long double weight) : m_gender(gender),
+                              m_size(size),
+                              m_weight(weight)
+    {
+        BOOST_TEST_MESSAGE(m_setup);
+        hr = new Human::Ratios(m_gender, m_size, m_weight);
+    }
+    ~TestRatiosFixt()
+    {
+        BOOST_TEST_MESSAGE(m_teardown);
+        delete hr;
+    }
+    Human::Ratios *hr;
+    unsigned int m_gender;
+    long double m_size;
+    long double m_weight;
+};
+
+struct MaleRatiosFixt : public TestRatiosFixt
+{
+    MaleRatiosFixt() : TestRatiosFixt(
+                              Human::Genders::Id::MALE,
+                              180,
+                              80) {}
+};
+
+struct FemaleRatiosFixt : public TestRatiosFixt
+{
+    FemaleRatiosFixt() : TestRatiosFixt(
+                                Human::Genders::Id::FEMALE,
+                                165,
+                                70) {}
+};
+
+BOOST_FIXTURE_TEST_CASE(MaleNormal, MaleRatiosFixt)
+{
+    for (const auto limbId : Human::Limbs::IdAll)
+    {
+        const long double value = hr->getWeight(limbId);
+        std::cout << std::setw(15) << std::left
+                  << Human::Limbs::Labels.at(limbId)
+                  << std::setw(2)
+                  << " : " << value << std::endl;
+    }
+
+    BOOST_CHECK_EQUAL(hr->getWeight(), 80);
+    BOOST_CHECK_EQUAL(hr->getSize(), 180);
 }
 
-BOOST_FIXTURE_TEST_CASE(THRF2, TestHumanRatiosFixture)
+BOOST_FIXTURE_TEST_CASE(FemaleNormal, FemaleRatiosFixt)
 {
-    BOOST_CHECK_EQUAL(i, 0);
-    BOOST_CHECK_EQUAL(4, 4);
+    BOOST_CHECK_EQUAL(hr->getWeight(), 70);
+    BOOST_CHECK_EQUAL(hr->getSize(), 165);
 }
 
 BOOST_AUTO_TEST_CASE(TestHumanRatiosDummy)
---
+++ test/testbasefixture.h
@@ -0,0 +1,11 @@
+
+#ifdef TEST_BASE_FIXTURE_H
+#define TEST_BASE_FIXTURE_H
+
+struct TestBaseFixture
+{
+    const std::string m_setup = "setup";
+    const std::string m_teardown = "teardown";
+};
+
+#endif
