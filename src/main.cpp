01a6459ca32b08093133195f4544bd8a572f511c
Pierre Fromager <info@pier-infor.fr>
2022-01-13 21:14:13 +0100

    feat : main display units

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,21 @@
 #define TAB "\t"
 #define STAR "*"
 #define SPACE " "
+#define CM "cm"
+#define CM2 CM "²"
+#define CM3 CM "³"
+#define KG "kg"
+#define WEIGHTS "weights"
+#define SIZES "sizes"
 
-void makeMePulse(Human::Ratios *hr)
+void pulse(Human::Ratios *hr)
 {
+    std::cout << "-- On pulse --" << std::endl;
     Human::Body *me = new Human::Body();
-    std::cout << "-- I pulse --" << std::endl;
     Ellipsoid *ell = new Ellipsoid();
     ld sumvol = 0;
     ld sumsur = 0;
-    me->propCallback = [hr, &ell, &sumvol, &sumsur](DLinkedNode *n) mutable {
+    me->propCallback = [hr, ell, &sumvol, &sumsur](DLinkedNode *n) mutable {
         const unsigned int id = n->getId();
         const ld size = hr->getSize(id);
         ell->setId(id);
@@ -30,17 +36,24 @@ void makeMePulse(Human::Ratios *hr)
         const ld s = ell->surface();
         std::cout << std::setw(18) << std::left
                   << Human::Limbs::Labels.at((Human::Limbs::Id)id)
-                  << std::setw(2)
-                  << v << "cm³ " << s << "cm²" << std::endl;
+                  << std::setw(7) << std::left
+                  << v << SPACE << CM3
+                  << std::left << std::setw(4)
+                  << SPACE << s << SPACE << CM2 << std::endl;
         sumvol += v;
         sumsur += s;
     };
     me->pulse();
     delete ell;
     delete me;
-    std::cout << "sum v : " << sumvol << "cm³" << std::endl;
-    std::cout << "sum s : " << sumsur << "cm²" << std::endl;
-    std::cout << "ratio s/v : " << sumsur / sumvol << std::endl;
+    std::cout << std::setw(18) << std::left << "SUM"
+              << std::setw(7) << std::left
+              << sumvol << SPACE << CM3
+              << std::left << std::setw(4)
+              << SPACE << sumsur << SPACE << CM2
+              << std::endl;
+    std::cout << std::setw(18) << std::left << "S/V"
+              << sumsur / sumvol << std::endl;
 }
 
 void reportHumanRatios(Human::Ratios *hr, bool weighted = true)
@@ -50,16 +63,21 @@ void reportHumanRatios(Human::Ratios *hr, bool weighted = true)
                                    : HUMAN_GENDER_LABEL_FEMALE;
     const std::string ssize = std::to_string((int)hr->getSize());
     const std::string sweight = std::to_string((int)hr->getWeight());
-    const std::string rtype = weighted ? "weights" : "sizes";
+    const std::string rtype = weighted ? WEIGHTS : SIZES;
     const std::string title = gender + SPACE + ssize + "/" + sweight + SPACE + rtype;
     std::cout << STAR << SPACE << title << std::endl;
+    char fmt_value[12];
     for (const auto limbId : Human::Limbs::IdAll)
     {
         const long double value = weighted ? hr->getWeight(limbId) : hr->getSize(limbId);
+        sprintf(fmt_value, "%5.2Lf", value);
+        const std::string unit = weighted ? KG : CM;
         std::cout << TAB
                   << std::setw(15) << std::left << Human::Limbs::Labels.at(limbId)
                   << std::setw(2)
-                  << " : " << value << std::endl;
+                  << SPACE << ":" << SPACE
+                  << fmt_value << SPACE << unit
+                  << std::endl;
     }
 }
 
@@ -70,10 +88,10 @@ int main(int argc, char **argv)
     if (prog)
     {
     }
-    Human::Ratios *hr = new Human::Ratios(Human::Genders::Id::MALE, 180, 80);
+    Human::Ratios *hr = new Human::Ratios(Human::Genders::Id::FEMALE, 170, 60);
     reportHumanRatios(hr);
     reportHumanRatios(hr, false);
-    makeMePulse(hr);
+    pulse(hr);
     delete hr;
     return 0;
 }
