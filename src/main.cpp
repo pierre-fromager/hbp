736c1b89e21677db18c47051d1b8108f4120bd12
Pierre Fromager <info@pier-infor.fr>
2022-01-14 19:14:42 +0100

    feat : console colors

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 
 #include <iostream>
 #include <iomanip>
-#include <stdexcept>
-#include "dlinkednode.h"
+#include "colors.h"
 #include "ellipsoid.h"
 #include "humanratios.h"
 #include "humanbody.h"
-#include "dlinkednode.h"
 
 #define TAB "\t"
 #define STAR "*"
@@ -33,42 +31,49 @@ typedef struct m_options_s
 
 void pulse(Human::Ratios *hr)
 {
-    std::cout << "-- On pulse --" << std::endl;
+    const Colors::Define c_title(Colors::Id::FG_YELLOW);
+    const Colors::Define c_label(Colors::Id::FG_CYAN);
+    const Colors::Define c_value(Colors::Id::FG_GREEN);
+    const Colors::Define c_reset(Colors::Id::RESET);
+    std::cout << c_title << STAR << SPACE << "Pulse"
+              << SPACE << "volume(" << CM3 << ")"
+              << SPACE << "surface(" << CM2 << ")"
+              << c_reset << std::endl;
     Human::Body *me = new Human::Body();
     Ellipsoid *ell = new Ellipsoid();
     ld sumvol = 0;
     ld sumsur = 0;
-    me->propCallback = [hr,
-                        ell,
-                        &sumvol,
-                        &sumsur](DLinkedNode *n) mutable {
+    me->propCallback = [c_title, c_label, c_value, c_reset,
+                        hr, ell,
+                        &sumvol, &sumsur](DLinkedNode *n) mutable {
         const unsigned int id = n->getId();
-        const ld size = hr->getSize(id);
+        const ld hsize = hr->getSize(id) * 0.5;
         ell->setId(id);
-        ell->setA(size * 0.5);
-        ell->setB(size * 0.5 * 0.5);
-        ell->setC(size * 0.5 * 0.4);
+        ell->setA(hsize);
+        ell->setB(hsize * 0.5);
+        ell->setC(hsize * 0.4);
         const ld v = ell->volume();
         const ld s = ell->surface();
-        std::cout << std::setw(18) << std::left
+        std::cout << TAB << c_label << std::setw(18) << std::left
                   << Human::Limbs::Labels.at((Human::Limbs::Id)id)
+                  << c_reset << c_value
                   << std::setw(7) << std::left
-                  << v << SPACE << CM3
-                  << std::left << std::setw(3)
-                  << SPACE << s << SPACE << CM2 << std::endl;
+                  << v << c_reset << SPACE
+                  << c_value << std::left << std::setw(3)
+                  << SPACE << s << c_reset << std::endl;
         sumvol += v;
         sumsur += s;
     };
     me->pulse();
     delete ell;
     delete me;
-    std::cout << std::setw(18) << std::left << "SUM"
-              << std::setw(7) << std::left
-              << sumvol << SPACE << CM3
-              << std::left << std::setw(3)
-              << SPACE << sumsur << SPACE << CM2
-              << std::endl;
-    std::cout << std::setw(18) << std::left << "S/V"
+
+    std::cout << TAB << c_label << std::setw(18) << std::left
+              << "SUM" << c_reset
+              << std::setw(7) << std::left << sumvol
+              << std::left << std::setw(4)
+              << SPACE << sumsur << std::endl;
+    std::cout << TAB << c_label << std::setw(18) << std::left << "S/V" << c_reset
               << sumsur / sumvol << std::endl;
 }
 
@@ -81,18 +86,23 @@ void reportHumanRatios(Human::Ratios *hr, bool weighted = true)
     const std::string sweight = std::to_string((int)hr->getWeight());
     const std::string rtype = weighted ? WEIGHTS : SIZES;
     const std::string title = gender + SPACE + ssize + "/" + sweight + SPACE + rtype;
-    std::cout << STAR << SPACE << title << std::endl;
+    const Colors::Define c_title(Colors::Id::FG_YELLOW);
+    const Colors::Define c_label(Colors::Id::FG_CYAN);
+    const Colors::Define c_value(Colors::Id::FG_GREEN);
+    const Colors::Define c_reset(Colors::Id::RESET);
+    const std::string unit = weighted ? KG : CM;
+    std::cout << c_title << STAR << SPACE << title
+              << SPACE << unit << c_reset << std::endl;
     char fmt_value[12];
     for (const auto limbId : Human::Limbs::IdAll)
     {
         const long double value = weighted ? hr->getWeight(limbId) : hr->getSize(limbId);
         sprintf(fmt_value, "%5.2Lf", value);
-        const std::string unit = weighted ? KG : CM;
-        std::cout << TAB
+        std::cout << TAB << c_label
                   << std::setw(15) << std::left << Human::Limbs::Labels.at(limbId)
+                  << c_reset << c_value
                   << std::setw(2)
-                  << SPACE << ":" << SPACE
-                  << fmt_value << SPACE << unit
+                  << SPACE << fmt_value << SPACE << c_reset
                   << std::endl;
     }
 }
